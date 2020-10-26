9d14bf8ef84731f8d1d1a9882d222b607ff14b98
Pascal Rocq <pascal.rocq+git@gmail.com>
2020-10-26 22:02:21 +0100

    Initial Commit

---
+++ GestionDeListe/GestionDeListeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestionDeListeApp.swift
+//  GestionDeListe
+//
+//  Created by Pascal Rocq on 26/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct GestionDeListeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
